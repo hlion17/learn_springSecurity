ca00cc771088f83a692a5745f0e4aae53908f549
hlion17 <yourdad20626@gmail.com>
2022-07-06 09:33:41 +0900

    사용자 정의 보안 설정 - 기존 WebSecurityConfigurerAdapter 방식 - spring security 5.7.0-M2 버전 이후   SecurityFilterChain을 빈으로 등록하는 방식으로 변경

---
+++ src/main/java/io/security/basicsecurity/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .formLogin();
+        return http.build();
+    }
+}
