6b4d5d82e2c92df5d3e9a2e1fac903ae2e801cb3
hlion17 <yourdad20626@gmail.com>
2022-07-07 01:43:14 +0900

    HttpSecurity API - Remember-Me

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,14 @@
+package io.security.basicsecurity;
+
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated();
+//        http
+//                .formLogin();
+//    }
+//}
